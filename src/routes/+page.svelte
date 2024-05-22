fc2ee24a4e21573ff35a256417f09039fc180247
dauid64 <carlosdauid64br@gmail.com>
2024-05-22 01:19:12 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="h-screen w-screen flex flex-col justify-center items-center">
+	<div class="card card-hover p-20 flex flex-col justify-center items-center">
+		<h1 class="font-bold text-5xl">TutorIA</h1>
+		<h2>Seu professor particular disponível toda hora.</h2>
+	</div>
+</div>
