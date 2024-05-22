cd1264869096ebc1d39d29a5e5f015cc0be84245
dauid64 <carlosdauid64br@gmail.com>
2024-05-22 15:31:31 -0300

    montando estrutura de páginas

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+    import { Icon, BookOpen, UserGroup, Cog6Tooth } from "svelte-hero-icons";
+    import { page } from '$app/stores';
+</script>
+
+<div class="flex">
+    <div class="h-screen">
+        <AppRail>
+            <svelte:fragment slot="lead">
+                <AppRailAnchor href="/dashboard" selected={$page.url.pathname === '/dashboard'}>
+                    <h1>TutorIA</h1>
+                </AppRailAnchor>
+            </svelte:fragment>
+            <AppRailAnchor href="/dashboard/alunos" selected={$page.url.pathname === '/dashboard/alunos'} target="_self" title="Alunos">
+                <svelte:fragment slot="lead">
+                    <div class="flex items-center justify-center">
+                        <Icon src="{UserGroup}" size="32" />
+                    </div>
+                </svelte:fragment>
+                <span>Alunos</span>
+            </AppRailAnchor>
+            <!-- --- -->
+            <AppRailAnchor href="/dashboard/materias" selected={$page.url.pathname === '/dashboard/materias'} target="_self" title="Materias">
+                <svelte:fragment slot="lead">
+                    <div class="flex items-center justify-center">
+                        <Icon src="{BookOpen}" size="32" />
+                    </div>
+                </svelte:fragment>
+                <span>Matérias</span>
+            </AppRailAnchor>
+            <!-- --- -->
+            <svelte:fragment slot="trail">
+                <AppRailAnchor href="/dashboard/configuracao" selected={$page.url.pathname === '/dashboard/configuracao'} target="_self" title="Configurações">
+                    <div class="flex items-center justify-center">
+                        <Icon src="{Cog6Tooth}" size="32" />
+                    </div>
+                </AppRailAnchor>
+            </svelte:fragment>
+        </AppRail>
+    </div>
+    
+    <slot></slot>
+</div>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let currentPage;
+</script>
+
+Página Principal
